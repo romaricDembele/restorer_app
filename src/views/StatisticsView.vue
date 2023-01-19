34da8524fc63b0103e8c50ef499972e965d938ee
Romaric DEMBELE <romadembele@gmail.com>
2023-01-19 18:05:59 +0100

    statistics OK

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -47,37 +47,45 @@
 
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 
-import axios from 'axios';
+// import axios from 'axios';
 // import {Menu, Restaurant} from '@/utils/interfaces';
 import {getOrders, getRestaurants} from '@/utils/helpers';
 
-export default {
-  data: () => ({
-    totalRevenue: 0,
-    numberOfOrders: 0,
-    topSellingMenuItem: '',
-    popularDeliveryLocation: '',
-    mostVisitedRestaurant: '',
-    averageOrderValue: 0
-  }),
-/*
+export default defineComponent({
+  data(){
+    return {
+      totalRevenue: 0,
+      numberOfOrders: 0,
+      topSellingMenuItem: '',
+      popularDeliveryLocation: '',
+      mostVisitedRestaurant: '',
+      averageOrderValue: 0
+    }
+  },
+
   created() {
-    getOrders.then(response => {
+    getOrders().then((response: any) => {
           // calculate total revenue
           this.totalRevenue = response.reduce((acc: any, order: any) => acc + order.order.total_cost, 0);
           // calculate number of orders
           this.numberOfOrders = response.length;
           // calculate top selling menu item
-          let menuItemCount = {};
+          // let menuItemCount = {};
+          let menuItemCount: { [name: string]: number } = {};
+
           response.forEach((order: any) => {
             order.order.menus.forEach((menu: any) => {
+
               menuItemCount[menu.name] = menuItemCount[menu.name] ? menuItemCount[menu.name] + 1 : 1;
             });
           });
           this.topSellingMenuItem = Object.keys(menuItemCount).reduce((a, b) => menuItemCount[a] > menuItemCount[b] ? a : b);
           // calculate popular delivery location
-          let deliveryCount = {};
+          // let deliveryCount = {};
+          let deliveryCount: { [name: string]: number } = {};
+
           response.forEach((order: any) => {
             deliveryCount[order.order.delivery_person.delivery_location] = deliveryCount[order.order.delivery_person.delivery_location] ? deliveryCount[order.order.delivery_person.delivery_location] + 1 : 1;
           });
@@ -87,28 +95,28 @@ export default {
           console.log(error)
       });
 
-      getRestaurants.then((response: any) => {
+      getRestaurants().then((response: any) => {
           // calculate most visited restaurant
-          let restaurantCount = {};
+          // let restaurantCount = {};
+          let restaurantCount: { [name: string]: number } = {};
           response.forEach((restaurant: any) => {
             restaurantCount[restaurant.name] = restaurantCount[restaurant.name] ? restaurantCount[restaurant.name] + 1 : 1;
           });
           this.mostVisitedRestaurant = Object.keys(restaurantCount).reduce((a, b) => restaurantCount[a] > restaurantCount[b] ? a : b);
           // calculate average order value
-          axios.get('api/restorer/orders')
-            .then((response: any) => {
+          getOrders().then((response: any) => {
                 this.averageOrderValue = this.totalRevenue / this.numberOfOrders;
             })
             .catch((error: any) => {
                 console.log(error)
             });
+            
       })
       .catch((error: any) => {
           console.log(error)
       });
   }
-  */
 
-}
+})
 
 </script>
